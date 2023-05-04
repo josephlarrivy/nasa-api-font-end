f64c8155c94847bc3cdbedc80e78b2360e2c52b6
josephlarrivy <joseph.larrivy@gmail.com>
2023-05-04 12:37:45 -0500

    ImageSearch done

--- src/components/ImageSearch.js
+++ src/components/ImageSearch.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Nav from "../common/Nav";
 
-import '../styles/Home.css'
 import '../styles/ImageSearch.css'
 import Footer from "../common/Footer";
 import NASA_DATA_REQUEST from "../api/api";
@@ -10,54 +9,83 @@ import { useParams } from "react-router-dom";
 const ImageSearch = () => {
 
   const {term} = useParams()
-  const [images, setImages] = useState(null)
+  const [imagesOuterArr, setImagesOuterArr] = useState(null)
+  const [selectedImage, setSelectedImage] = useState(null);
+
 
   useEffect(() => {
     console.log(term)
     const getData = async () => {
       const data = await NASA_DATA_REQUEST.queryImages(term)
-      console.log(data)
+      // console.log(data)
 
       let imagesArr = []
+      let imagesSubset = []
 
       for (let item of data) {
         if (item.links && item.links[0] && item.links[0].href) {
-          // console.log(item.links[0].href);
-          // console.log(item.data[0].title);
-          // console.log(item.data[0].description);
-
-          imagesArr.push(
-            {
-              'title': item.data[0].title,
-              'description': item.data[0].description,
-              'link': item.links[0].href
-            }
-          )
+          imagesSubset.push({
+            'title': item.data[0].title,
+            'description': item.data[0].description,
+            'link': item.links[0].href
+          })
+          if (imagesSubset.length === 5) {
+            console.log(imagesSubset)
+            imagesArr.push(imagesSubset)
+            imagesSubset = []
+          }
         }
       }
-      setImages(imagesArr)
+      if (imagesSubset.length === 5) {
+        imagesArr.push(imagesSubset)
+      }
+
+      setImagesOuterArr(imagesArr)
     }
     getData()
   }, [])
 
+
   return (
-    <div id="home-container">
-      <Nav navColor='white' textColor='black' />
+    <div id="images-page-container">
+      <Nav navColor='black' textColor='white' />
       <h1>{term.toUpperCase()} IMAGES</h1>
       <div className="search-photos-container">
-        {images && images.map(image => {
+        {imagesOuterArr && imagesOuterArr.map(innerArr => {
           return (
-            <div className="search-photo-container">
-                <img src={image.link} alt={image.description} />
-                <p><strong>Title</strong></p>
-                <p>{image.title}</p>
-                <p><strong>Description</strong></p>
-                <p>{image.description}</p>
+            <div className="search-photos-group-container">
+              <div className="image-one">
+                <img src={innerArr[0].link} onClick={() => setSelectedImage(innerArr[0].link)} />
+              </div>
+              <div className="search-photos-group-inner-right-container">
+                <div className="images-right-top">
+                  <div className="image-two">
+                    <img src={innerArr[1].link} onClick={() => setSelectedImage(innerArr[1].link)} />
+                  </div>
+                  <div className="image-three">
+                    <img src={innerArr[2].link} onClick={() => setSelectedImage(innerArr[2].link)} />
+                  </div>
+                </div>
+                <div className="images-right-bottom">
+                  <div className="image-four">
+                    <img src={innerArr[3].link} onClick={() => setSelectedImage(innerArr[3].link)} />
+                  </div>
+                  <div className="image-five">
+                    <img src={innerArr[4].link} onClick={() => setSelectedImage(innerArr[4].link)} />
+                  </div>
+                </div>
+              </div>
             </div>
           )
         })}
       </div>
-      <Footer navColor='white' textColor='black' />
+      {selectedImage && (
+        <div className="image-overlay">
+          <img src={selectedImage} />
+          <button onClick={() => setSelectedImage(null)} id='close-overlay-button'>Close Image</button>
+        </div>
+      )}
+      <Footer navColor='black' textColor='white' />
     </div>
   )
 }
